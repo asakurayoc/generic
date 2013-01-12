c5232f89d4db79668c03a7b546afd4a971338146
Asakura Yo <noppadon.char@gmail.com>
2013-01-13 02:40:42 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mybbs::Application.config.secret_token = '101364d69bf219dfa43ac214441c1c299cb455fe85b5bc529bdc626daaff3f2465d182a8009ace76903ce55381696a0ab2ffff4901602e6bafb3afad1fcba218'
